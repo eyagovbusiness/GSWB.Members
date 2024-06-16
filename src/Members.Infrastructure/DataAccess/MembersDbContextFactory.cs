47c8768ecd6ff70a4b6bc4f23b4fcf8ef1583d3a
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-06-16 15:25:19 +0200

    Renamed DbFactory for design time.

--- src/Members.Infrastructure/DataAccess/MembersDbContextFactory.cs
+++ src/Members.Infrastructure/DataAccess/MembersDbContextFactory.cs
@@ -10,7 +10,7 @@ namespace Members.Infrastructure
     /// WARNING: This factory should ONLY be used in a development environment for design-time operations.
     /// Production or Staging connection strings will never be used for design-time operations. 
     /// </remarks>
-    public class AuthorizationDbContextFactory : PostgreSQLDesignTimeDbContextFactory<MembersDbContext>
+    public class MembersDbContextFactory : PostgreSQLDesignTimeDbContextFactory<MembersDbContext>
     {
     }
 
